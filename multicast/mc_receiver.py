a2fb8e1649376815e01ee61f9520ae3fa420ca71
kanar11 <kanarekkasper@gmail.com>
2026-04-10 18:49:56 +0000

    feat: add multicast sender/receiver for market data simulation

---
+++ multicast/mc_receiver.py
@@ -0,0 +1,20 @@
+import socket
+import struct
+import time
+
+MCAST_GROUP = '239.1.1.1'
+MCAST_PORT = 5001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('', MCAST_PORT))
+
+mreq = struct.pack('4sL', socket.inet_aton(MCAST_GROUP), socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+print(f"Listening on {MCAST_GROUP}:{MCAST_PORT}...")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    recv_time = time.time_ns()
+    print(f"[{recv_time}] From {addr}: {data.decode()}")
---
+++ multicast/mc_sender.py
@@ -0,0 +1,16 @@
+import socket
+import time
+
+MCAST_GROUP = '239.1.1.1'
+MCAST_PORT = 5001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+seq = 0
+while True:
+    msg = f"SEQ={seq} TS={time.time_ns()}"
+    sock.sendto(msg.encode(), (MCAST_GROUP, MCAST_PORT))
+    print(f"Sent: {msg}")
+    seq += 1
+    time.sleep(0.1)
